efb9c6be8cfc4b6bf5e3350d48936d55017b1d16
Userasma <136679245+Userasma@users.noreply.github.com>
2023-08-02 14:49:53 +0400

    Update the dependencies

--- initializers/database.go
+++ initializers/database.go
@@ -7,10 +7,11 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
-//In order to make DB accessible from other packages 
-var DB *gorm.DB 
 
-//to set up the Gorm database connection
+// In order to make DB accessible from other packages
+var DB *gorm.DB
+
+// to set up the Gorm database connection
 func ConnectToDB() {
 	dsn := os.Getenv("DB_URL")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
